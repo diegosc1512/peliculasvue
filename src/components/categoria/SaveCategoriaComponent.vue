f4f2a46b0d621bcb2d1b9d4aacdb6d050054d278
unknown <diegosc1512@gmail.com>
2022-10-23 16:22:43 -0400

    buscador en terminado

--- src/components/categoria/SaveCategoriaComponent.vue
+++ src/components/categoria/SaveCategoriaComponent.vue
@@ -5,21 +5,21 @@
     <h1>Listado de categorias en componente</h1>
 
     <input type="text" v-model="buscar" class="form-control" placeholder="Ejemplo: terror"/>
-
+    
     <div class="row mt-3">
     
-    <div class="col-md-4" v-for="item in categorias" v-bind:key="item.id"> 
+        <div class="col-md-4" v-for="item in items" v-bind:key="item.id"> 
 
-        <div class="card mb-3">
-            <!--<img class="card-img-top" v-bind:src="'img/' + item.img" v-bind:alt="item.nombre">-->
-            <div class="card-body">
-            <h3 class="card-title mb-3"><strong>Nombre:</strong>{{ item.nombre }}</h3>
-            <p class="card-text"><strong>Descripcion:</strong> {{ item.descripcion }}</p>
-   
+            <div class="card mb-3">
+                <!--<img class="card-img-top" v-bind:src="'img/' + item.img" v-bind:alt="item.nombre">-->
+                <div class="card-body">
+                <h3 class="card-title mb-3"><strong>Nombre:</strong>{{ item.nombre }}</h3>
+                <p class="card-text"><strong>Descripcion:</strong> {{ item.descripcion }}</p>
+    
+                </div>
             </div>
         </div>
     </div>
-</div>
         <div v-for="c in categorias" k:ey="c.id">
             <div >Nombre: {{c.nombre}} </div>
             <div >Descripcion: {{c.descripcion}} </div>
@@ -170,9 +170,8 @@
      computed: {
 
         items() {
-      return categorias.filter(categorias => {
-        return categoria.nombre.toLowerCase().includes(this.buscar.toLowerCase());
-        console.log( categoria.nombre.toLowerCase().includes(this.buscar.toLowerCase()));
+      return this.categorias.filter(item => {
+        return item.nombre.toLowerCase().includes(this.buscar.toLowerCase());
       });
     },
      },
